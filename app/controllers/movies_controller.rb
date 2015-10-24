60cd210d65914aafa404bc7812bc88f2e562281b
ctayl138 <ctayl138@uncc.edu>
2015-10-24 00:07:31 +0000

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,9 +10,45 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
-  def index
-    @movies = Movie.all
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
   end
+
+  def index
+
+    @all_ratings = ['G','PG','PG-13','R']
+    
+    if params[:ratings]
+      session[:ratings] = params[:ratings]  
+    elsif !params[:ratings] && !session[:ratings]
+      session[:ratings] = {'G' => 1,'PG'=> 1,'PG-13'=> 1,'R'=> 1}
+    end
+    
+    @checked_ratings = Array.new
+    session[:ratings].each_key {|key| @checked_ratings.push(key)}
+    
+    if session[:sort] == 'title' && params[:sort] == nil || params[:sort] == 'title'
+      sort = 'title'
+      @title_header = 'hilite'
+    elsif (session[:sort] == "release_date" && params[:sort] == nil) || params[:sort] == "release_date"
+      sort = 'release_date'
+      @release_date_header = 'hilite'
+    elsif
+      sort = 1
+    end
+    
+    @movies = Movie.order(sort).where(rating: @checked_ratings)
+    
+    if (session[:sort] != params[:sort] && params[:sort] != nil) || (session[:ratings] != params[:ratings] && params[:ratings] != nil)
+        session[:sort] = params[:sort] 
+        session[:ratings] = params[:ratings]
+        flash.keep
+        redirect_to movies_path(:sort => session[:sort], :ratings => @checked_ratings) and return
+    end
+  end
+
 
   def new
     # default: render 'new' template
